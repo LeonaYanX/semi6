f7bb0a5da3aa2c97261e73c5a5218c7a9078c149
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2024-01-22 22:15:19 +0400

    Tasksss

--- Program.cs
+++ Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив символов (тип char []). Создайте строку из
+// символов этого массива.
+// Указание
+// Конструктор строки вида string(char []) не использовать.
+// Пример
+// [‘a’, ‘b’, ‘c’, ‘d’] => “abcd”
+
+
+//char[] array = new char[] { 't', 'y', '3', 'r', '$', };
+//string str = "";
+
+//for (int i = 0; i < array.Length; i++)
+//{
+//    str = str + array[i];
+//}
+
+//System.Console.WriteLine(str);
+// На основе символов строки (тип string) сформировать массив
+// символов (тип char[]). Вывести массив на экран.
+// Указание
+// Метод строки ToCharArray() не использовать.
+// Пример
+// “Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’ ] 
+
+//System.Console.WriteLine("Введите строчку");
+//string str = Console.ReadLine();
+//char[] array = new char[str.Length];
+
+//for (int i = 0; i < array.Length; i++)
+//{
+//  array[i] = str[i];
+//System.Console.Write(array[i] + "");
+//}
+// Считать строку с консоли, состоящую из латинских
+// букв в нижнем регистре. Выяснить, сколько среди
+// введённых букв гласных.
+// Пример
+// “hello” => 2
+// “world” => 1
+
+//string userInput = Console.ReadLine();
+//string vowels = "aeiyuo";
+
+//int countVowels = 0;
+
+//for (int i = 0; i < userInput.Length; i++)
+//{
+    //for (int j = 0; j < vowels.Length; j++)
+    //{
+       // if (userInput[i].ToString().ToLower() == vowels[j].ToString())
+       // {
+           // countVowels++;
+           // }
+           // }
+           //}
+           //System.Console.WriteLine(countVowels);
+
+// char ch = 'a'; // символ для проверки
+// bool isLetter = Char.IsLetter(ch); // проверяем, является ли символ буквой
+// bool isVowel = "aeiou".Contains(ch); // проверяем, является ли символ гласной
+
+
